a1c7a627dbe68e1df718820e8fc3443d39dbd84f
Eneko Illarramendi <eneko@illarra.com>
2026-03-06 13:56:40 +0100

    chore: separate gunicorn config

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn hipeac_mcp.server:app --workers 1 --worker-class uvicorn_worker.UvicornWorker --bind 0.0.0.0:$PORT
+web: gunicorn hipeac_mcp.server:app --config gunicorn.config.py
 worker: huey_consumer.py hipeac_mcp.tasks.huey -w 2 -q
---
+++ gunicorn.config.py
@@ -0,0 +1,9 @@
+import os
+
+
+bind = f":{os.environ.get('PORT', 5000)}"
+workers = int(os.environ.get("GUNICORN_WORKERS", 1))
+worker_class = "uvicorn_worker.UvicornWorker"
+timeout = int(os.environ.get("GUNICORN_TIMEOUT", 30))
+graceful_timeout = int(os.environ.get("GUNICORN_GRACEFUL_TIMEOUT", 30))
+keepalive = int(os.environ.get("GUNICORN_KEEPALIVE", 5))
